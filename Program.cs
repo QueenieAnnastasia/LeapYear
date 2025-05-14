e65ab0dac3f04193dfc94aee330a6498c98e2172
QueenieAnnastasia <94047113+QueenieAnnastasia@users.noreply.github.com>
2025-05-14 21:09:27 +0700

    Tính năm nhuận

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Nhập năm cần tính");
+        int yearcount = Convert.ToInt32(Console.ReadLine());
+        if (yearcount%4 != 0)
+        {
+            Console.WriteLine("Năm " + yearcount + " không phải là năm nhuận");
+        }
+        else
+        {
+            if (yearcount%100 == 0 && yearcount%400 != 0)
+            {
+                Console.WriteLine("Năm " + yearcount + " không phải là năm nhuận");
+            }
+            else
+            {
+                Console.WriteLine("Năm " + yearcount + " là năm nhuận");
+            }
+        }
+    }
+}
